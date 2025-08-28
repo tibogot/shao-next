23885faaa01fa893bd697421dd051c70bee4b123
tibogot <tibogotbeatz@gmail.com>
2025-08-28 03:59:50 +0200

    third

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -8,8 +8,6 @@ export function useLenis() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: "vertical",
-      gestureDirection: "vertical",
       smooth: true,
       smoothTouch: false,
       touchMultiplier: 2,
