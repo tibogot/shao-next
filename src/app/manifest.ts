64b7783f10336c6e8c7cccaa0dc42e1b4a41b697
tibogot <tibogotbeatz@gmail.com>
2025-08-31 04:18:24 +0200

    g

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -20,7 +20,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/logo.svg",
         sizes: "any",
         type: "image/svg+xml",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/logo.svg",
